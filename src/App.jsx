e603f0a6f83dd89f4a0338044608d520a15823aa
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-01-07 11:53:17 +0500

    dfs

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ function App() {
       <Hero />
       <Main />
       <Footer />
+      <div className="flex justify-center pb-12">
+        <p className="text-[#6B7280]">© 2025 Blogify. All rights reserved.</p>
+      </div>
     </>
   );
 }
